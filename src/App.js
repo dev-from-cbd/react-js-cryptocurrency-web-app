8ed2147b4eab74fba663540827cfe502da0bf7bb
dev-from-cbd <veryfatwombat@gmail.com>
2024-01-13 09:30:17 +1030

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import React from "react";
-
 const App = () => {
-  return <div>App</div>;
+  return <div>React JS Cryptocurrency Web App</div>;
 };
 
 export default App;
-
-//import logo from "./logo.svg";
-//import "./App.css";
-//function App() {
-// return (
-//  <div className="App">
-//   <header className="App-header">
-//    <img src={logo} className="App-logo" alt="logo" />
-//   <p>React JS Cryptocurrency Web App</p>
-//  Redux Toolkit
-//</header>
-//</div>
-//);
-//}
-//export default App;
