8dbea884f0cd592307fa47a645fa1d305d0f1819
dev-from-cbd <veryfatwombat@gmail.com>
2024-01-14 09:00:53 +1030

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+//import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
 const App = () => {
-  return <div>React JS Cryptocurrency Web App</div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <h1>NavBar - React JS Cryptocurrency Web App</h1>
+      </div>
+      <div className="main">Main - React JS Cryptocurrency Web App</div>
+    </div>
+  );
 };
 
 export default App;
