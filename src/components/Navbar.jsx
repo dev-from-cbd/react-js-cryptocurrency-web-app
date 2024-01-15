5ef867ddc6f56103101c58821b3164ddd7318b8e
dev-from-cbd <veryfatwombat@gmail.com>
2024-01-16 07:27:01 +1100

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,26 @@
 //import React from "react";
-import { Button, Menu, Typography } from "antd";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar />
+        <Typography.Title lavel={2} className="logo">
+          <Link to="/">LOGO - React Cryptocurrency App</Link>
+        </Typography.Title>
+        <Button className="menu-control-container">Button</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
